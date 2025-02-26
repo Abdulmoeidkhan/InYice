826376d6e470693fc557990e8081eef00074311a
hamkhan <hamkhan088@gmail.com>
2025-02-26 16:09:26 +0500

    changes made for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
     plugins: [
@@ -9,5 +10,34 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+            manifest: {
+              name: "Meri Laravel React App",
+              short_name: "ReactPWA",
+              description: "Yeh Laravel + React + Vite PWA hai.",
+              theme_color: "#ffffff",
+              background_color: "#ffffff",
+              display: "standalone",
+              start_url: "/",
+              icons: [
+                {
+                  src: "/icons/icon-192x192.png",
+                  sizes: "192x192",
+                  type: "image/png",
+                },
+                {
+                  src: "/icons/icon-512x512.png",
+                  sizes: "512x512",
+                  type: "image/png",
+                },
+              ],
+            },
+            devOptions: {
+              enabled: true,
+              type: "module",
+            },
+          }),
     ],
 });
